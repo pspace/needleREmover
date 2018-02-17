2b3ba5a9db28538ec459b02d15403343e36fc356
pspace <hendrik@polylog.space>
2018-02-17 13:57:08 +0100

    first commit

---
+++ app/src/main/java/space/polylog/owasp/needleremover/SSLActivity.java
@@ -0,0 +1,16 @@
+package space.polylog.owasp.needleremover;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import needleremover.R;
+
+
+public class SSLActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ssl);
+    }
+}
