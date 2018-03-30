65e0215a863ed6d41477b932305f553aa8ed2bc7
pspace <hendrik@polylog.space>
2018-03-31 01:58:35 +0200

    post Talk

---
+++ app/src/main/java/space/polylog/owasp/needleremover/Native_Call.java
@@ -0,0 +1,13 @@
+package space.polylog.owasp.needleremover;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Native_Call extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_native__call);
+    }
+}
